2636c662ba8dcfa3e9170d44e3d5cc7d8fc3210e
LBosshard <bosshard@nexus.ethz.ch>
2021-11-15 11:04:40 +0100

    add man

--- R/f_my_wilcox_test.R
+++ R/f_my_wilcox_test.R
@@ -1,3 +1,16 @@
+#' Wilcox test
+#'
+#' Calculate wilcox p-value or return preset values
+#'
+#' @param x value 1
+#' @param y value 2
+#' @param min_genes minimum genes
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' f_my_wilcox_test(rnorm(10,1,2),rnorm(10,5,2))
 f_my_wilcox_test <- function(x, y, min_genes = 5) {
   if (length(x) >= min_genes & length(y) >= min_genes) {
     return(wilcox.test(x, y, alternative = "greater")$p.value)
